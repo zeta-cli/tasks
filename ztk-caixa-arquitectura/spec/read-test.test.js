957608e225545d11a29200808d5656c76810095f
Antonio Hermosilla Parra <ahermosi@everis.com>
2020-01-16 08:26:48 +0100

    Changes test ztk-caixa-arquitectura

---
+++ ztk-caixa-arquitectura/spec/read-test.test.js
@@ -0,0 +1,11 @@
+const { readTest } = require('../src/index');
+
+
+test('Testing read test task caixa-arquitectura.readTest', (done) => {
+
+  readTest({}, { source: `${process.cwd()}/assets/class/**/*.java` }).then(result => {
+    console.log(JSON.stringify(result, null, 2));
+    done();
+  });
+
+});
